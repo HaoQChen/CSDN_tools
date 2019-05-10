79d9e95fcf74a14b7607b64c199513c23ef03168
CalamityChen <1218138462@qq.com>
2019-05-10 10:52:02 +0800

    update20190510

--- auto_run.py
+++ auto_run.py
@@ -7,19 +7,21 @@ use_github = True
 
 def main():
     current_path = os.path.dirname(os.path.realpath(__file__))
+    #将远程内容更新到本地
     if use_github:
         repo = Repo(current_path)
-        repo.remote().pull()#将远程内容更新到本地
+        repo.remote().pull()
+
     #运行相关脚本
     os.system("python3 " + current_path + "/CSDN_visited_num.py")
+
+    #推送到github
     if use_github:
         #得到当前日期
         now_time = str(datetime.datetime.now().strftime('%Y%m%d'))
-        # repo.index.add("-A")
-        # repo.index.commit("update " + now_time)
         repo.git.add("-A")
         repo.git.commit("-m", "update" + now_time)
-        repo.remote().push()
+        repo.remote("origin").push()
 
 if __name__ == "__main__":
     main()
